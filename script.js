7ce3c2bd677facbda7bd0d61a46fe09e3f991d65
hazembensaria <bensariahazem@gmail.com>
2024-05-07 13:03:17 +0100

    add key functionality and add some comments

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
-let x  = document.getElementsByClassName("liverpool")
+/**
+ * here we are selecting the dom element from the dom tree using some selectors like "getElementById"
+ */
+let imageList  = document.getElementsByClassName("liverpool")
 let next  = document.getElementById('next')
 let menuButton  = document.getElementById('menuButton')
 let menu  = document.getElementById('menu')
@@ -10,16 +13,28 @@ let bottom = document.getElementById("bottom")
 let tow = document.getElementById("tow")
 let auto = document.getElementById("auto")
 playButton = document.getElementById("playButton")
-
+shuffleButton = document.getElementById("shuffle")
+/***
+ * this boolean  variables are controlling the menu displaying and function autodisplay
+*/
 let isDisplayAuto = false
 show = true
+
 let index = 0
+
+/**
+ * this list is for controlling images with menu buttons
+ */
 let imagesSepareted = [
     "Liverpool1",    "Liverpool2",    "Liverpool3",    "Liverpool4",    "Liverpool5", "Liverpool",
     "chelsea1", "chelsea2", "chelsea3", "chelsea4", "chelsea5", "chelsea6",
     "unt1", "unt2","unt3","unt4","unt5","unt6",
     "city1","city2","city3","city4","city5","city6"
 ]
+
+/**
+ * this 2d list is for displaying the caroussel without the menu 
+ */
 let images = [
     ["Liverpool1",    "Liverpool2",    "Liverpool3",    "Liverpool4",    "Liverpool5", "Liverpool"],
     ["chelsea1", "chelsea2", "chelsea3", "chelsea4", "chelsea5", "chelsea6"],
@@ -36,11 +51,14 @@ function changeImage(){
 function nextImages(){
     index == 3 ? index = 0 : index++ ;
  for(j=0 ; j<6 ; j++){
-    x[j].src = "images/"+images[index][j]+".jpeg"
-    // console.log(images[index][j])
+    imageList[j].src = "images/"+images[index][j]+".jpeg"
 }
-image.src = x[0].src
+image.src = imageList[0].src
   }
+  /**
+   * this function is for showing and hiding the menu 
+   * also some animatons
+   */
 function showMenu(){
         console.log("this is show menu function");
         if(!bottom.classList.contains("moveDown")
@@ -62,7 +80,9 @@ function showMenu(){
         }
 }
 l=0
-
+/** 
+ * this function auto change the main image
+ */
 function displayAuto(){
     if(!isDisplayAuto){
         playButton.classList.remove("fa-play")
@@ -82,30 +102,75 @@ function displayAuto(){
  isDisplayAuto = !isDisplayAuto
 
 }
-
+/**
+ * 
+ * this function show the next 6 images in the caroussel 
+ */
   function previousImages(){
       index == 0 ? index = 3 : index-- ;
     for(j=0 ; j<6 ; j++){
-       x[j].src = "images/"+images[index][j]+".jpeg"
-       // console.log(images[index][j])
+        imageList[j].src = "images/"+images[index][j]+".jpeg"
+
    }
-   image.src = x[0].src
+   image.src = imageList[0].src
      }
 
-for(i=0 ; i< x.length ; i++){
-    x[i].addEventListener("click" ,changeImage)
+     /**
+      * this function show the previous 6 images in the caroussel
+      */
+   function  nextSeparatedImage(){
+         l== imagesSepareted.length-1 ? l=0 : l++
+        image.src =  "images/"+imagesSepareted[l]+".jpeg"
+  
+     }
+
+     /**
+      * 
+      * this function show the next separeted image in the carrousel
+      */
+     function  previousSeparatedImage(){
+        l== 0 ? l=imagesSepareted.length-1 : l--
+       image.src =  "images/"+imagesSepareted[l]+".jpeg"
+ 
+    }
+    /**
+     * this function show a random image from the caroussel 
+     */
+     function pickRandomImage(){
+        rand = Math.floor(Math.random()*imagesSepareted.length)
+        l= rand
+        image.src = "images/"+imagesSepareted[rand]+".jpeg"
+
+     }
+/** 
+ * here we are adding the "click" event to the image list 
+ */
+for(i=0 ; i< imageList.length ; i++){
+    imageList[i].addEventListener("click" ,changeImage)
 }
-document.addEventListener("keypress" , (e)=>{
-    if(e.code === "Space")
+/** 
+ * here we are adding the "keypress" event to controll the caroussel 
+ */
+document.addEventListener("keypress" , (event)=>{
+    if(event.key === "ArrowLeft")
+      {
+       console.log("jsfklk")
+        previousSeparatedImage()}
+    if(event.code === "Space")
         {displayAuto()}
+     if(event.key ==="ArrowRight")
+        {nextSeparatedImage()}
+ 
 
-})   
+})     
+  /**   
+   * here we are adding events to DOM elements that we selected 
+   */
 next.addEventListener("click" ,nextImages)
-one.addEventListener("click" ,nextImages)
-tow.addEventListener("click" ,previousImages)
-
+one.addEventListener("click" ,previousSeparatedImage)
+tow.addEventListener("click" ,nextSeparatedImage)
+shuffleButton.addEventListener("click", pickRandomImage)
 previous.addEventListener("click" ,previousImages)
-
 menuButton.addEventListener("click" ,showMenu)
 auto.addEventListener("click" ,displayAuto)
  
